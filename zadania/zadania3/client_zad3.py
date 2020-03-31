c503ce742ec488b41114a9934b7c018efbab4578
Oleksandr Kotyk <oleksandr.kotyk@icloud.com>
2020-03-31 11:05:27 +0200

    Delete Zadania end

--- zadania/zadania3/client_zad3.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-
-s = socket(AF_INET, SOCK_DGRAM)
-
-s.settimeout(1)
-m = input("Your message: ")
-s.sendto(m.encode("utf-8"), ("127.0.0.1", 12645))
-m = s.recvfrom(1024)
-print("From: ", m[0].decode("utf-8"))
--- zadania/zadania3/server_zad3.py
+++
@@ -1,9 +0,0 @@
-from socket import *
-import datetime
-
-s = socket(AF_INET, SOCK_DGRAM)
-s.bind(("127.0.0.1", 12645))
-
-while True:
-    m = s.recvfrom(1024)
-    s.sendto(m[0], m[1])
